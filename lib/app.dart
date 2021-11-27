c7444187922a27e4d1722c7971bc066276dd0573
benz93chung <benz93chung@gmail.com>
2021-11-28 03:07:16 +0800

    Added status page + removed unwanted widgets

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pseudosejahtera/screens/status_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme(
           brightness: Brightness.light,
-          background: Colors.white70,
+          background: Colors.grey.shade300,
           onBackground: Colors.white,
           primary: const Color(0xFF3983FE),
           primaryVariant: const Color(0xFF2E64BF),
@@ -24,54 +25,7 @@ class MyApp extends StatelessWidget {
           onError: Colors.white,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const StatusPage(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 import 'package:pseudosejahtera/app.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      await FlutterStatusbarcolor.setStatusBarColor(const Color(0xFF3983FE));
+
+      runApp(const MyApp());
+    },
+    (_, __) {},
+  );
 }
